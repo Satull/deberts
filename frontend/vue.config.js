92de7be8a1d944833d3b117f26299197c78cdbaa
Ievgenii Izrailtenko <i.izrailtenko@web.de>
2021-03-07 22:47:11 +0100

    Dev-Server Configuration

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-    // proxy all webpack dev-server requests starting with /api
-    // see https://cli.vuejs.org/config/#devserver-proxy
     devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8080', 
-          ws: true,
-          changeOrigin: true
+        proxy: {
+            '/': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            }
         }
-      }
     },
-    // Change build paths to make them Maven compatible
-    // see https://cli.vuejs.org/config/
     outputDir: 'target/dist',
     assetsDir: 'static'
-  };
+};
